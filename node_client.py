ae0985fe902fba7dd3a7b3f394efb06c00527e00
Sean Simons <f00356z@dartmouth.edu>
2020-05-30 21:40:07 -0600

    Updated code

--- node_client.py
+++ node_client.py
@@ -1,5 +1,26 @@
+import sys
+sys.path.append('../')
 from Node import Node
 
 node = Node()
-node.node_open('127.0.0.1', 8081)
-node.node_connect('127.0.0.1', 8080)
+print('Enter IP')
+myip = input('')
+print('Enter Port')
+myport = int(input(''))
+node.node_open(myip, myport)
+while True:
+	msg = input('')
+	if msg == 'Q':
+		break
+	elif msg[0:2] == 'C ':
+		msg_parts = msg[2:].split(' ')
+		node.node_connect(msg_parts[0], int(msg_parts[1]))
+	elif msg[0:2] == 'T ':
+		node.node_chat(msg[2:])
+	elif msg[0:2] == 'B ':
+		node.node_broadcast_file(msg[2:])
+	elif msg == 'G':
+		node.node_get_files()
+	elif msg[0:2] == 'R ':
+		node.node_request_file(msg[2:])
+node.node_close()
